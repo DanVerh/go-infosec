7df666e4255fe3e8274da130e900d25b2099bc6c
DanVerh <danverh29@gmail.com>
2023-09-28 23:59:47 +0300

    code finished

--- lab2/lab2.1/main.go
+++ lab2/lab2.1/main.go
@@ -6,12 +6,11 @@ var message = "ЧЕГДОМИН 5000 Т"
 var gamma = "БІЛОМИР"
 
 func main() {
-	fmt.Println(getLength(gamma))
-	fmt.Println(getLength(message))
-	fmt.Println(getPartsAmount(message, gamma))
-	fmt.Println(alphabetMap())
-	fmt.Println(gammaLengthToMeassage(message, gamma))
+	fmt.Println("Message: " + message)
+	fmt.Println("Gamma: " + gamma + "\n")
 
+	gamma = gammaLengthToMessage(message, gamma)
+	fmt.Println("Encrypted message: " + encrypt(message, gamma))
 }
 
 func getLength(text string) (length int) {
@@ -28,25 +27,6 @@ func getPartsAmount(message string, gamma string) (parts int) {
 	} else {
 		return getLength(message)/getLength(gamma) + 1
 	}
-}
-
-func gammaLengthToMeassage(message string, gamma string) (editedGamma string) {
-	parts := getPartsAmount(message, gamma)
-	if parts > 1 {
-		for p := 0; p <= parts; p++ {
-			editedGamma = gamma + gamma
-		}
-	}
-	lettersLeft := getLength(message) - getLength(editedGamma)
-	for i, r := range gamma {
-		if i <= lettersLeft-1 {
-			editedGamma += fmt.Sprintf("%c", r)
-		}
-		break
-	}
-	/*for i, u := range message {}*/
-
-	return editedGamma
 }
 
 func alphabetMap() (m map[string]int) {
@@ -87,49 +67,58 @@ func alphabetMap() (m map[string]int) {
 	return m
 }
 
-/*
-func alphabetMap() (m map[int]string) {
-	m = make(map[int]string)
-	var unicode int32
-	count := 0
-	for unicode = 1040; unicode <= 1048; unicode++ {
-		m[count] = fmt.Sprintf("%c", unicode)
-		count++
+func gammaLengthToMessage(message string, gamma string) (editedGamma string) {
+	parts := getPartsAmount(message, gamma)
+	if parts > 1 {
+		for p := 0; p <= parts; p++ {
+			editedGamma = gamma + gamma
+		}
 	}
-	m[count] = fmt.Sprintf("%c", 1030)
-	count++
-	m[count] = fmt.Sprintf("%c", 1111)
-	count++
-	for unicode = 1049; unicode <= 1071; unicode++ {
-		m[count] = fmt.Sprintf("%c", unicode)
-		count++
+	lettersLeft := getLength(message) - getLength(editedGamma)
+	for i, r := range gamma {
+		if i <= lettersLeft-1 {
+			editedGamma += fmt.Sprintf("%c", r)
+		}
+		break
 	}
-	m[count] = fmt.Sprintf("%c", 32)
-	count++
-	for unicode = 48; unicode <= 57; unicode++ {
-		m[count] = fmt.Sprintf("%c", unicode)
-		count++
-	}
-	return m
+
+	return editedGamma
 }
-*/
-/*func alphabetSlice() (alphabet []string) {
-	var unicode int32
-	var symbol string
-	for unicode = 1040; unicode <= 1048; unicode++ {
-		symbol = fmt.Sprintf("%c", unicode)
-		alphabet = append(alphabet, symbol)
+
+func getKeyByValue(m map[string]int, value int) string {
+	for key, v := range m {
+		if v == value {
+			return key // Found the key for the value
+		}
 	}
-	alphabet = append(alphabet, fmt.Sprintf("%c", 1030))
-	alphabet = append(alphabet, fmt.Sprintf("%c", 1111))
-	for unicode = 1049; unicode <= 1071; unicode++ {
-		symbol = fmt.Sprintf("%c", unicode)
-		alphabet = append(alphabet, symbol)
+	return "" // Value not found in the map
+}
+
+func encrypt(message string, gamma string) string {
+	key := alphabetMap()
+	var ecryptedMessage []int
+	var ecryptedGamma []int
+	var encryptedSum []int
+	var ecryptedText string
+
+	for _, r := range message {
+		ecryptedMessage = append(ecryptedMessage, key[fmt.Sprintf("%c", r)])
 	}
-	alphabet = append(alphabet, fmt.Sprintf("%c", 32))
-	for unicode = 48; unicode <= 57; unicode++ {
-		symbol = fmt.Sprintf("%c", unicode)
-		alphabet = append(alphabet, symbol)
+
+	for _, r := range gamma {
+		ecryptedGamma = append(ecryptedGamma, key[fmt.Sprintf("%c", r)])
 	}
-	return alphabet
-}*/
+
+	for i := range ecryptedMessage {
+		encryptedSum = append(encryptedSum, ecryptedMessage[i]+ecryptedGamma[i])
+		if encryptedSum[i] > 44 {
+			encryptedSum[i] = encryptedSum[i] - 44
+		}
+	}
+
+	for i := range encryptedSum {
+		ecryptedText += getKeyByValue(key, encryptedSum[i])
+	}
+
+	return ecryptedText
+}
