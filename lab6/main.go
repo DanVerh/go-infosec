4d63fb4e7953d7f0482daef2aa231b0f84a0da9c
DanVerh <danverh29@gmail.com>
2023-12-06 04:29:15 +0200

    big int experiment

--- lab6/main.go
+++ lab6/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/big"
 	"math/rand"
 	"time"
 )
 
+var n = 128
+var t = 10
+
 func main() {
-	p := generateRandomInt()
-	b := findMaxPowerOfTwo(p - 1)
+	p := generateRandomBigInt(n)
+	fmt.Println(p)
+	b := findMaxPowerOfTwo(new(big.Int).Sub(p, big.NewInt(64)))
+	fmt.Println(b)
 	m := getM(p, b)
-	a := generateRandomIntLessP(p)
-	j := 0
+	fmt.Println(m)
+	a := generateRandomBigIntLessP(n, p)
+	fmt.Println(a)
+	j := big.NewInt(0)
+	fmt.Println(j)
 	z := getZ(a, m, p)
+	fmt.Println(z)
 	fmt.Printf("p = %d, b = %d, m = %d, a = %d, j = %d, z = %d \n", p, b, m, a, j, z)
 	testResult := testRabinMiller(z, j, p, b)
 	fmt.Println(testResult)
@@ -24,55 +33,99 @@ func generateRandomInt() int {
 	return rand.Intn(100) + 2
 }
 
-func findMaxPowerOfTwo(n int) int {
-	b := 0
-	for n%2 == 0 {
-		n /= 2
-		b++
+func generateRandomBigInt(bitLength int) *big.Int {
+	// Generate random bytes
+	randomBytes := make([]byte, (bitLength+7)/8)
+
+	// Create a big.Int from the random bytes
+	randomBigInt := new(big.Int).SetBytes(randomBytes)
+
+	// Set the top bit to ensure the number is positive and larger than 2^64
+	minValue := new(big.Int).Exp(big.NewInt(2), big.NewInt(64), nil)
+
+	randomBigInt.Add(randomBigInt, minValue)
+	randomBigInt.SetBit(randomBigInt, bitLength-1, 1)
+
+	randomBigInt.SetBit(randomBigInt, 0, 1)
+
+	return randomBigInt
+}
+
+func findMaxPowerOfTwo(n *big.Int) *big.Int {
+	b := new(big.Int)
+	two := big.NewInt(2)
+
+	// Iterate while n is even
+	for n.Bit(0) == 0 { // Check if the least significant bit is 0
+		n.Div(n, two)           // Divide n by 2
+		b.Add(b, big.NewInt(1)) // Increment the count of powers of 2
 	}
 	return b
 }
 
-func getM(p, b int) int {
-	return (p - 1) / int(math.Pow(2, float64(b)))
+func getM(p, b *big.Int) *big.Int {
+	subp1 := new(big.Int).Sub(p, big.NewInt(1))
+	exp := new(big.Int).Exp(big.NewInt(2), b, nil)
+	result := new(big.Int).Div(subp1, exp)
+
+	return result
 }
 
-func generateRandomIntLessP(p int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(p-1) + 2
+func generateRandomBigIntLessP(bitLength int, upperLimit *big.Int) *big.Int {
+	for {
+		// Generate random bytes
+		randomBytes := make([]byte, (bitLength+7)/8)
+		_, err := rand.Read(randomBytes)
+		if err != nil {
+			panic(err) // Handle the error appropriately in your actual code
+		}
+
+		// Create a big.Int from the random bytes
+		randomBigInt := new(big.Int).SetBytes(randomBytes)
+
+		// Check if the generated value is less than the upper limit
+		if randomBigInt.Cmp(upperLimit) < 0 {
+			return randomBigInt
+		}
+	}
 }
 
-func getZ(a, m, p int) int {
-	am := int(math.Pow(float64(a), float64(m)))
-	return am - p*(am/p)
+func getZ(a, m, p *big.Int) *big.Int {
+	// Calculate a^m
+	am := new(big.Int).Exp(a, m, nil)
+
+	// Calculate result: am - p*(am/p)
+	amDivP := new(big.Int).Div(am, p)
+	result := new(big.Int).Sub(am, new(big.Int).Mul(p, amDivP))
+
+	return result
 }
 
-func testRabinMiller(z, j, p, b int) (result bool) {
+func testRabinMiller(z, j, p, b *big.Int) bool {
 	// point 3
-	if z == 1 || z == p-1 {
-		result = true
-		return
+	one := big.NewInt(1)
+	pMinusOne := new(big.Int).Sub(p, one)
+	if z.Cmp(one) == 0 || z.Cmp(pMinusOne) == 0 {
+		return true
 	}
 
 	// point 5
-	j += 1
-	if j < b && z < p-1 {
-		z2 := int(math.Pow(float64(z), 2))
-		z = z2 - p*(z2/p)
-		if j > 0 && z == 1 {
-			result = false
-			return
-		} else if z == p-1 {
-			result = true
-			return
+	j.Add(j, one)
+	bigB := new(big.Int).Set(b)
+	if j.Cmp(bigB) < 0 && z.Cmp(pMinusOne) < 0 {
+		z2 := new(big.Int).Exp(z, big.NewInt(2), nil)
+		z.Mod(z2.Sub(z2, p.Mul(z2.Div(z2, p), p)), p)
+		if j.Cmp(one) > 0 && z.Cmp(one) == 0 {
+			return false
+		} else if z.Cmp(pMinusOne) == 0 {
+			return true
 		}
 	}
 
 	// point 6
-	if j == b && z != p-1 {
-		result = false
-		return
+	if j.Cmp(bigB) == 0 && z.Cmp(pMinusOne) != 0 {
+		return false
 	}
 
-	return result
+	return false
 }
